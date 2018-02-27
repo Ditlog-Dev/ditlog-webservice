31ee7f25bdc0fa5879bee3261164b9cb4aa84515
Dery Rahman A <dery.ra@gmail.com>
2018-02-27 18:15:03 +0700

    Initial commit

---
+++ src/main/java/id/ac/itb/logistik/ditlog/DitlogApplication.java
@@ -0,0 +1,12 @@
+package id.ac.itb.logistik.ditlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DitlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DitlogApplication.class, args);
+	}
+}
