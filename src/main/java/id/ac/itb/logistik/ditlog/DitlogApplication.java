7253cea0af6b84b5345e3c36c14e58267a713e53
Ray Andrew <13515073@std.stei.itb.ac.id>
2018-03-02 13:25:32 +0700

    change docker environment

--- src/main/java/id/ac/itb/logistik/ditlog/DitlogApplication.java
+++ src/main/java/id/ac/itb/logistik/ditlog/DitlogApplication.java
@@ -3,15 +3,16 @@ package id.ac.itb.logistik.ditlog;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class DitlogApplication {
 
-	@RequestMapping("/")
+	@RequestMapping(value="/", method= RequestMethod.GET)
 	public String home() {
-			return "Hello Docker Worldsss";
+			return "Hello Docker Worlds";
 	}
 
 	public static void main(String[] args) {
