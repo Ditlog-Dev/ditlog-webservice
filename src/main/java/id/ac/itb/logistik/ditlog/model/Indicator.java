fde1194c7e72f895037c2cacf3be5156c37c732b
Dery Rahman A <dery.ra@gmail.com>
2018-03-02 14:04:38 +0700

    Add JPA, finish get all indicators from db

--- src/main/java/id/ac/itb/logistik/ditlog/model/Indicator.java
+++ src/main/java/id/ac/itb/logistik/ditlog/model/Indicator.java
@@ -1,14 +1,30 @@
 package id.ac.itb.logistik.ditlog.model;
 
-import javax.persistence.Entity;
 
-/**
- * Not implemented yet. Just an example
- */
+import javax.persistence.*;
+import java.util.Date;
+
 @Entity
+@Table(name = "INDIKATOR_PENILAIAN")
 public class Indicator {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "ID_INDIKATOR")
     Long id;
+
+    @Column(name = "NAMA_INDIKATOR")
     String name;
+
+    @Column(name = "TGL_BUAT")
+    Date dateCreated;
+
+    @Column(name = "TGL_UPDATE")
+    Date dateModified;
+
+    @Column(name = "ID_USER")
+    Long idUser;
+
 
     public Long getId() {
         return id;
@@ -24,5 +40,29 @@ public class Indicator {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Date getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(Date dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    public Date getDateModified() {
+        return dateModified;
+    }
+
+    public void setDateModified(Date dateModified) {
+        this.dateModified = dateModified;
+    }
+
+    public Long getIdUser() {
+        return idUser;
+    }
+
+    public void setIdUser(Long idUser) {
+        this.idUser = idUser;
     }
 }
