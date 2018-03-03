00dc50cfce93fa63c6b35438f3c1722b5dcd866a
nim_13515118 <gianffh@gmail.com>
2018-03-03 14:26:44 +0700

    Init user repository

---
+++ src/main/java/id/ac/itb/logistik/ditlog/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package id.ac.itb.logistik.ditlog.repository;
+
+import id.ac.itb.logistik.ditlog.model.Indicator;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<Indicator, Long> {
+}
+
