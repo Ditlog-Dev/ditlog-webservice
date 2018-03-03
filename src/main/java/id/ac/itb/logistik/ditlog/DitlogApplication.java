59f867038f8540c04f5596ed27203acf4343c927
Ray Andrew <13515073@std.stei.itb.ac.id>
2018-03-03 09:07:27 +0700

    add test executable for CI

--- src/main/java/id/ac/itb/logistik/ditlog/DitlogApplication.java
+++ src/main/java/id/ac/itb/logistik/ditlog/DitlogApplication.java
@@ -3,20 +3,18 @@ package id.ac.itb.logistik.ditlog;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class DitlogApplication {
 
-	@GetMapping("/")
-	public String init(){
-		return "Web Service Ditlog API";
-	}
+  @GetMapping("/")
+  public String init() {
+    return "Web Service Ditlog API";
+  }
 
-	public static void main(String[] args) {
-		SpringApplication.run(DitlogApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(DitlogApplication.class, args);
+  }
 }
--- src/test/java/id/ac/itb/logistik/ditlog/DitlogApplicationTests.java
+++ src/test/java/id/ac/itb/logistik/ditlog/DitlogApplicationTests.java
@@ -1,16 +1,24 @@
 package id.ac.itb.logistik.ditlog;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class DitlogApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+  @Autowired
+  private TestRestTemplate restTemplate;
+
+  @Test
+  public void contextLoads() {
+    String body = this.restTemplate.getForObject("/", String.class);
+    Assert.assertEquals(body, "Web Service Ditlog API");
+  }
 
 }
