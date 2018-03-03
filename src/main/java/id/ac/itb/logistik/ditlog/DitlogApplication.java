8916ca16d0627dc505ea6c2fabdf61ba851da328
nim_13515118 <gianffh@gmail.com>
2018-03-03 13:47:42 +0700

    Fix timezone issue when connecting to Oracle

--- src/main/java/id/ac/itb/logistik/ditlog/DitlogApplication.java
+++ src/main/java/id/ac/itb/logistik/ditlog/DitlogApplication.java
@@ -6,9 +6,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 @RestController
 public class DitlogApplication {
+
 
 	@GetMapping("/")
 	public String init(){
@@ -16,6 +19,9 @@ public class DitlogApplication {
 	}
 
 	public static void main(String[] args) {
+		TimeZone timeZone = TimeZone.getTimeZone("America/Los_Angeles"); // e.g. "Europe/Rome"
+		TimeZone.setDefault(timeZone);
+
 		SpringApplication.run(DitlogApplication.class, args);
 	}
 }
