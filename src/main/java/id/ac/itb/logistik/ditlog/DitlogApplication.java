57b277bf8956c8f6a84e12fe861a1d0f3f1aca4f
nim_13515118 <gianffh@gmail.com>
2018-03-03 20:37:20 +0700

    Add authorization filter

--- src/main/java/id/ac/itb/logistik/ditlog/DitlogApplication.java
+++ src/main/java/id/ac/itb/logistik/ditlog/DitlogApplication.java
@@ -1,7 +1,10 @@
 package id.ac.itb.logistik.ditlog;
 
+import id.ac.itb.logistik.ditlog.config.JwtFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +14,15 @@ import java.util.TimeZone;
 @SpringBootApplication
 @RestController
 public class DitlogApplication {
+
+	@Bean
+	public FilterRegistrationBean jwtFilter() {
+		final FilterRegistrationBean registrationBean = new FilterRegistrationBean();
+		registrationBean.setFilter(new JwtFilter());
+		registrationBean.addUrlPatterns("/");
+
+		return registrationBean;
+	}
 
 	@GetMapping("/")
 	public String init(){
