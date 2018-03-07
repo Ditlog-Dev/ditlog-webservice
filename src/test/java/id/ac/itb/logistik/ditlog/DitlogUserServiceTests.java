174d86990abf3e9f5c3513ce3d548c7f2fb18ae5
Kevin Erdiza Yogatama <kyogatama@gmail.com>
2018-03-07 10:33:20 +0700

    updated unit testing to add its own dummy data

--- src/test/java/id/ac/itb/logistik/ditlog/DitlogUserServiceTests.java
+++ src/test/java/id/ac/itb/logistik/ditlog/DitlogUserServiceTests.java
@@ -1,14 +1,17 @@
 package id.ac.itb.logistik.ditlog;
 
 import id.ac.itb.logistik.ditlog.model.User;
+import id.ac.itb.logistik.ditlog.repository.UserRepository;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.*;
@@ -18,43 +21,38 @@ import javax.xml.bind.DatatypeConverter;
 import java.security.MessageDigest;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class DitlogUserServiceTests {
-
     @Autowired
     private TestRestTemplate restTemplate;
 
+    @Autowired
+    UserRepository userRepo;
+
+    @LocalServerPort
+    private int port;
+
     private HttpHeaders headers = new HttpHeaders();
 
-    private static final String API_ROOT = "http://localhost:8080/user";
+    private User testUser;
 
-    private ResponseEntity<String> getStringResponseEntity(User user) {
-        HttpEntity<User> entity = new HttpEntity<>(user, headers);
-
-        return restTemplate.exchange(
-                API_ROOT,
-                HttpMethod.POST, entity, String.class
-        );
+    @Before
+    public void setUp() {
+        testUser = new User();
+        testUser.setUsername("john");
+        testUser.setPassword(MD5Encrypt("456"));
+        testUser.setIdUser(2L);
+        testUser.setEmail("aep@doe.com");
+        testUser.setIdEmployee(2L);
+        userRepo.save(testUser);
     }
 
-    private String MD5Encrypt(String password) {
-        try {
-            MessageDigest md = MessageDigest.getInstance("MD5");
-            md.update(password.getBytes());
-            byte[] digest = md.digest();
-            return DatatypeConverter
-                    .printHexBinary(digest).toLowerCase();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
 
     @Test
     public void whenPostCorrectAuth_thenOK() {
         User user = new User();
-        user.setUsername("aep");
-        user.setPassword(MD5Encrypt("123"));
+        user.setUsername(testUser.getUsername());
+        user.setPassword(MD5Encrypt(testUser.getPassword()));
 
         ResponseEntity<String> response = getStringResponseEntity(user);
 
@@ -71,22 +69,22 @@ public class DitlogUserServiceTests {
     @Test
     public void whenPostCorrectAuth_thenGetCorrectId() {
         User user = new User();
-        user.setUsername("aep");
-        user.setPassword(MD5Encrypt("123"));
+        user.setUsername(testUser.getUsername());
+        user.setPassword(MD5Encrypt(testUser.getPassword()));
 
         ResponseEntity<String> response = getStringResponseEntity(user);
 
         JSONObject responseJson;
         try {
             responseJson = new JSONObject(response.getBody());
-            int actualIdUser;
+            long actualIdUser;
             Object payload = responseJson.optJSONObject("payload");
             if(payload != null){
-                actualIdUser = ((JSONObject)payload).getInt("idUser");
+                actualIdUser = ((JSONObject)payload).getLong("idUser");
             } else {
                 actualIdUser = -1;
             }
-            Assert.assertEquals(2,actualIdUser);
+            Assert.assertEquals(2L,actualIdUser);
         } catch (JSONException e) {
             e.printStackTrace();
             Assert.fail();
@@ -96,8 +94,8 @@ public class DitlogUserServiceTests {
     @Test
     public void whenPostCorrectAuth_thenGetCorrectJwtToken() {
         User user = new User();
-        user.setUsername("aep");
-        user.setPassword(MD5Encrypt("123"));
+        user.setUsername(testUser.getUsername());
+        user.setPassword(MD5Encrypt(testUser.getPassword()));
 
         ResponseEntity<String> response = getStringResponseEntity(user);
 
@@ -144,5 +142,31 @@ public class DitlogUserServiceTests {
     @Test
     public void checkMD5Encryption() {
         Assert.assertEquals("202cb962ac59075b964b07152d234b70", MD5Encrypt("123"));
+    }
+
+    private ResponseEntity<String> getStringResponseEntity(User user) {
+        HttpEntity<User> entity = new HttpEntity<>(user, headers);
+
+        return restTemplate.exchange(
+                createURLWithPort("/user"),
+                HttpMethod.POST, entity, String.class
+        );
+    }
+
+    private String MD5Encrypt(String password) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            md.update(password.getBytes());
+            byte[] digest = md.digest();
+            return DatatypeConverter
+                    .printHexBinary(digest).toLowerCase();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private String createURLWithPort(String uri) {
+        return "http://localhost:" + port + uri;
     }
 }
