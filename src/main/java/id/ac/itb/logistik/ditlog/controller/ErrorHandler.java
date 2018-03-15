0a2ad7edf38417dca8553060ddc241e42252ea23
Dery Rahman A <dery.ra@gmail.com>
2018-03-15 22:42:33 +0700

    Implement exception handling

--- src/main/java/id/ac/itb/logistik/ditlog/controller/ErrorHandler.java
+++ src/main/java/id/ac/itb/logistik/ditlog/controller/ErrorHandler.java
@@ -1,20 +1,38 @@
 package id.ac.itb.logistik.ditlog.controller;
 
 import id.ac.itb.logistik.ditlog.model.BaseResponse;
+import javax.persistence.EntityNotFoundException;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
+@RestController
 public class ErrorHandler extends ResponseEntityExceptionHandler {
 
-  @ExceptionHandler(Exception.class)
-  protected ResponseEntity<BaseResponse> handleGeneralException(Exception ex) {
+  public ErrorHandler() {
+    super();
+  }
+
+  @ExceptionHandler({ Exception.class })
+  public ResponseEntity<BaseResponse> handleGeneralException(Exception ex) {
     BaseResponse baseResponse = new BaseResponse();
     baseResponse.setStatus(false);
-    baseResponse.setCode(500);
+    baseResponse.setCode(HttpStatus.BAD_REQUEST.value());
     baseResponse.setMessage(ex.getMessage());
-    return ResponseEntity.badRequest().body(baseResponse);
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST.value()).body(baseResponse);
+  }
+
+  @ExceptionHandler(EntityNotFoundException.class)
+  public ResponseEntity<BaseResponse> handleNotFoundException(RuntimeException ex) {
+    BaseResponse baseResponse = new BaseResponse();
+    baseResponse.setStatus(false);
+    baseResponse.setCode(HttpStatus.NOT_FOUND.value());
+    baseResponse.setMessage(ex.getMessage() + " not found");
+    return ResponseEntity.status(HttpStatus.NOT_FOUND.value()).body(baseResponse);
   }
 }
